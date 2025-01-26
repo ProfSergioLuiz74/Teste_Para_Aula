e4a5be5c72b4859d4c79be03080bf24ec20da379
Sergio Luiz <serluiz_1974@hotmail.com>
2025-01-26 20:53:07 -0300

    deploy inicial
    Primeiro commit enviado

---
+++ Exe02.py
@@ -0,0 +1,6 @@
+primeironome = input("Entre com o primeiro nome")
+segundonome = input("Entre com o segundo nome")
+nome = primeironome + ' ' + segundonome
+length = len(nome)
+print (nome)
+print(length)
---
+++ Exe03.py
@@ -0,0 +1,7 @@
+primeironome = input("Entre com o primeiro nome: ")
+segundonome = input("Entre com o segundo nome: ")
+primeironome = primeironome.title()
+segundonome = segundonome.title()
+nome = primeironome + " " + segundonome
+print(nome)
+
