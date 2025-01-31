ac155e0218c60c7491da20eefcbff019bd8f406d
Sergio Luiz <serluiz_1974@hotmail.com>
2025-01-31 15:09:03 -0300

    Arquivo Teste01
    Novo arquivo adicionado na branch

---
+++ teste01.py
@@ -0,0 +1,21 @@
+print("1. Vermelho")
+print("2. Laranja")
+print("3. Amarelo")
+print("4. Verden")
+print("5. Azule")
+print("6. Roxo")
+Choice = int(input("Selecione sua cor favorita: "))
+if (Choice == 1):
+    print("ocê escolheu Vermelho!")
+elif (Choice == 2):
+    print("Você escolheu Laranja!")
+elif (Choice == 3):
+    print("Você escolheu Amarelo!")
+elif (Choice == 4):
+    print("Você escolheu  Verde!")
+elif (Choice == 5):
+    print("Você escolheu Azul!")
+elif (Choice == 6):
+    print("Você escolheu Roxople!")
+else:
+    print("Você fez uma escolha inválida!")
