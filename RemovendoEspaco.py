4ce12fc25f5c8c7aedc7cbf9c62a668eb82b5226
Sergio Luiz <serluiz_1974@hotmail.com>
2025-02-09 20:18:20 -0300

    Arquivos do SLIDE
    exemplos do SLIDE.

---
+++ RemovendoEspaco.py
@@ -0,0 +1,15 @@
+nome = "sergio"
+sobrenome = "Luiz"
+email = 'sergio@gmail.com'
+
+print("Não modificado:")
+print(sobrenome.casefold())
+
+print("\nUsando lstrip():")
+print(nome.capitalize())
+
+print("\nPegando posição ZERO no email atribuido:")
+print(email[0])
+
+print("\nPegando da posição ZERO até a SETE do email atribuido:")
+print(email[0:7])
