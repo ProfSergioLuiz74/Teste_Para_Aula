dc76e2933aecea7cbe33b8a8e41271886d2e1f96
Sergio Luiz <serluiz_1974@hotmail.com>
2025-02-09 21:29:03 -0300

    adicao de mais exemplos

---
+++ ConcatenandoVariaveis.py
@@ -0,0 +1,5 @@
+faturamento = 1000
+print('Para variavel faturamento da loja foi:',faturamento)
+
+faturamento = 1000
+print('Para variavel faturamento da loja foi: '+str(faturamento))
---
+++ MetodoLen.py
@@ -0,0 +1,4 @@
+email = 'SergioLuiz@gmail.com'
+
+print("\nUsando metodo LEN para saber o tamanho da variavel email:")
+print(len(email))
