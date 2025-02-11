0c21c1e5ad358f0d6a33de8224b27ce718c87c74
Sergio Luiz <serluiz_1974@hotmail.com>
2025-02-11 08:47:53 -0300

    Introdução a IF
    Exemplo de IF

---
+++ UsoIF.py
@@ -0,0 +1,7 @@
+num1 = int(input("Digite um numero: "))
+if num1 >10:
+    print("Este Numero é maior que 10")
+elif num1 == 10:
+    print("Este Numero é igual a 10")
+else:
+    print("Este Numero é menor que 10")
