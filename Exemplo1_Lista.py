469ce5319d85c613110d8c6695d56cffa0e29176
Sergio Luiz <serluiz_1974@hotmail.com>
2025-02-12 16:52:17 -0300

    Lista

--- Exemplo1_Lista.py
+++ Exemplo1_Lista.py
@@ -1,2 +1,18 @@
 # A ESTRUTURA DA LISTA É ESSA:
-Nome_da_Lista = ['item 1', 'item 2', 'item 3']  
+Nome_da_Lista = ['item 1', 'item 2', 'item 3'] 
+
+
+Nome_da_Lista = [] # Lista vazia
+
+Lista1 = [1,2,3,4,5]
+Lista2 = [6,7,8,9,10]
+Lista3 = [11,12,13,14,15]
+todas_Listas = [Lista1, Lista2, Lista3] 
+print(todas_Listas)
+
+
+Nome_da_Lista = [
+    'item1',
+    'item2',
+    'item3']
+
