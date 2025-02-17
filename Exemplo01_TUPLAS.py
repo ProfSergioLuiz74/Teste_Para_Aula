0835cb44f46b20a39ff76deaf94432a105b8d39c
Sergio Luiz <serluiz_1974@hotmail.com>
2025-02-17 12:04:37 -0300

    Create Exemplo01_TUPLAS.py

---
+++ Exemplo01_TUPLAS.py
@@ -0,0 +1,11 @@
+vendas = ('Lira','25/08/2020','15/02/1994',2000, 'Estagiario')
+print(vendas)
+venda = ['Lira','25/08/2020','15/02/1994',2000,'Estagiario']
+print(venda)
+# nome = vendas[0]
+# data_contratacao = vendas[1]
+# data_nascimento = vendas[2]
+# salario = vendas[3]
+# cargo = vendas[4]
+
+
