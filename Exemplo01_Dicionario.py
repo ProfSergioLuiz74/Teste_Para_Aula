42113e6d0b23782f7b473b8c6efd887ffe41dc12
Sergio Luiz <serluiz_1974@hotmail.com>
2025-02-18 22:24:17 -0300

    Exemplos Dicionarios

---
+++ Exemplo01_Dicionario.py
@@ -0,0 +1,7 @@
+mais_vendidos = {'tecnologia':'iphone','refrigeracao':'ar consul 1200 btu','livros':'o alquimista','eletrodomesticos':'geladeira'}
+
+vendas_tecnologia = {'iphone':15000,'samsung galaxy':12000,'Tv Samsung':10000,'ps5':14300,'tablet':1720,'ipad':1200}	
+livro = mais_vendidos['livros']
+print(mais_vendidos.get('livros'))
+print(mais_vendidos['livros'])
+print('Fim programa')
---
+++ Exemplo02_Dicionario.py
@@ -0,0 +1,9 @@
+mais_vendidos = {'tecnologia':'iphone','refrigeracao':'ar consul 1200 btu','livros':'o alquimista','eletrodomesticos':'geladeira'}
+livro = mais_vendidos['livros']
+print(livro)
+
+
+
+
+
+#Exemplo02_Dicionario.py
