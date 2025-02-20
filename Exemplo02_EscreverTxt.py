8dbf9903ea3b3be2a2c4d798ad1b5bf6c1af6f72
Sergio Luiz <serluiz_1974@hotmail.com>
2025-02-20 09:54:59 -0300

    Arquivo TXT

---
+++ Exemplo02_EscreverTxt.py
@@ -0,0 +1,8 @@
+arquivo2 = open('E:/Senai-SC - 2023/002 - Aulas/015 - Python/002 - Aulas Sergio - SLIDE/LerTXT/GOT.txt', 'w')
+arquivo2.write('Escrevendo no arquivo\n')
+arquivo2.write('John Snow\n')
+arquivo2.write('Arya Stark\n')
+arquivo2.write('Sansa Stark\n')
+arquivo2.write('Bran Stark\n')
+arquivo2.close()
+print('Fim programa')
---
+++ Exemplo03_EscreverTxtWhit.py
@@ -0,0 +1,4 @@
+with open('E:/Senai-SC - 2023/002 - Aulas/015 - Python/002 - Aulas Sergio - SLIDE/LerTXT/Resumo 3.txt','w') as arquivo3:
+    arquivo3.write('Escrevendo no arquivo\n')
+    arquivo3.write('Capitão Stoltomayer\n')
+print('Fim programa')
